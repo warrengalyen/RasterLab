53e95ef31b88e67ade6616e7618f71e01eebcd15
warrengalyen <wgalyen@hotmail.com>
2025-12-10 08:39:07 -0800

    Create filter dialog template

---
+++ src/ui/widgets/filter_dialog.c
@@ -0,0 +1,482 @@
+#include "ui/widgets/filter_dialog.h"
+#include "render/layer.h"
+#include "render/compositor.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * Filter dialog structure
+ */
+struct _FilterDialog {
+    GtkWidget *dialog;
+    FilterPreview *preview;
+    GtkWidget *controls_box;
+    GtkWidget **scale_widgets;      /* Array of scale widgets */
+    GtkWidget **spin_widgets;        /* Array of spin button widgets */
+    FilterControlParam *params;      /* Copy of control parameters */
+    gint num_controls;
+    FilterDialogPreviewCallback preview_callback;  /* Callback for live preview */
+    gpointer preview_user_data;      /* User data for preview callback */
+};
+
+/**
+ * Scale value changed callback - updates spin button and triggers preview
+ */
+static void on_scale_value_changed(GtkRange *range, gpointer user_data)
+{
+    FilterDialog *dialog = (FilterDialog *)user_data;
+    GtkWidget *spin_button;
+    gdouble value;
+    gdouble *values;
+    gint i;
+
+    if (!dialog) {
+        return;
+    }
+
+    /* Find which control this scale belongs to */
+    for (i = 0; i < dialog->num_controls; i++) {
+        if (dialog->scale_widgets[i] == GTK_WIDGET(range)) {
+            spin_button = dialog->spin_widgets[i];
+            if (spin_button) {
+                value = gtk_range_get_value(range);
+                gtk_spin_button_set_value(GTK_SPIN_BUTTON(spin_button), value);
+            }
+
+            /* Trigger preview update if callback is set */
+            if (dialog->preview_callback) {
+                values = (gdouble *)g_malloc(sizeof(gdouble) * dialog->num_controls);
+                if (values) {
+                    filter_dialog_get_values(dialog, values, dialog->num_controls);
+                    dialog->preview_callback(dialog, values, dialog->num_controls, dialog->preview_user_data);
+                    g_free(values);
+                }
+            }
+            break;
+        }
+    }
+}
+
+/**
+ * Spin button value changed callback - updates scale and triggers preview
+ */
+static void on_spin_value_changed(GtkSpinButton *spin, gpointer user_data)
+{
+    FilterDialog *dialog = (FilterDialog *)user_data;
+    GtkWidget *scale;
+    gdouble value;
+    gdouble *values;
+    gint i;
+
+    if (!dialog) {
+        return;
+    }
+
+    /* Find which control this spin button belongs to */
+    for (i = 0; i < dialog->num_controls; i++) {
+        if (dialog->spin_widgets[i] == GTK_WIDGET(spin)) {
+            scale = dialog->scale_widgets[i];
+            if (scale) {
+                value = gtk_spin_button_get_value(spin);
+                gtk_range_set_value(GTK_RANGE(scale), value);
+            }
+
+            /* Trigger preview update if callback is set */
+            if (dialog->preview_callback) {
+                values = (gdouble *)g_malloc(sizeof(gdouble) * dialog->num_controls);
+                if (values) {
+                    filter_dialog_get_values(dialog, values, dialog->num_controls);
+                    dialog->preview_callback(dialog, values, dialog->num_controls, dialog->preview_user_data);
+                    g_free(values);
+                }
+            }
+            break;
+        }
+    }
+}
+
+/**
+ * Reset button clicked callback
+ */
+static void on_reset_clicked(GtkWidget *widget, gpointer user_data)
+{
+    FilterDialog *dialog = (FilterDialog *)user_data;
+    (void)widget;
+    filter_dialog_reset(dialog);
+}
+
+/**
+ * Create a new filter dialog
+ */
+FilterDialog* filter_dialog_new(const gchar *title,
+                                 const FilterControlParam *controls,
+                                 gint num_controls)
+{
+    FilterDialog *dialog;
+    GtkWidget *content_area;
+    GtkWidget *main_hbox;
+    GtkWidget *controls_vbox;
+    GtkWidget *reset_button;
+    GtkWidget *button_box;
+    gint i;
+
+    if (!title || !controls || num_controls <= 0) {
+        return NULL;
+    }
+
+    dialog = (FilterDialog *)g_malloc(sizeof(FilterDialog));
+    if (!dialog) {
+        return NULL;
+    }
+
+    /* Allocate arrays for widgets */
+    dialog->scale_widgets = (GtkWidget **)g_malloc(sizeof(GtkWidget *) * num_controls);
+    dialog->spin_widgets = (GtkWidget **)g_malloc(sizeof(GtkWidget *) * num_controls);
+    dialog->params = (FilterControlParam *)g_malloc(sizeof(FilterControlParam) * num_controls);
+
+    if (!dialog->scale_widgets || !dialog->spin_widgets || !dialog->params) {
+        g_free(dialog->scale_widgets);
+        g_free(dialog->spin_widgets);
+        g_free(dialog->params);
+        g_free(dialog);
+        return NULL;
+    }
+
+    /* Copy control parameters */
+    memcpy(dialog->params, controls, sizeof(FilterControlParam) * num_controls);
+    dialog->num_controls = num_controls;
+
+    /* Initialize widget arrays */
+    for (i = 0; i < num_controls; i++) {
+        dialog->scale_widgets[i] = NULL;
+        dialog->spin_widgets[i] = NULL;
+    }
+
+    /* Create dialog window */
+    dialog->dialog = gtk_dialog_new_with_buttons(title,
+                                                  NULL,
+                                                  GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
+                                                  "_OK",
+                                                  GTK_RESPONSE_OK,
+                                                  "_Cancel",
+                                                  GTK_RESPONSE_CANCEL,
+                                                  NULL);
+    
+    gtk_window_set_default_size(GTK_WINDOW(dialog->dialog), 750, 550);
+    gtk_window_set_resizable(GTK_WINDOW(dialog->dialog), TRUE);
+
+    /* Get content area */
+    content_area = gtk_dialog_get_content_area(GTK_DIALOG(dialog->dialog));
+    gtk_container_set_border_width(GTK_CONTAINER(content_area), 5);
+
+    /* Create main horizontal box */
+    main_hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
+    gtk_container_add(GTK_CONTAINER(content_area), main_hbox);
+
+    /* Create filter preview widget (left side) */
+    dialog->preview = FILTER_PREVIEW(filter_preview_new());
+    gtk_widget_set_size_request(GTK_WIDGET(dialog->preview), 400, -1);
+    gtk_box_pack_start(GTK_BOX(main_hbox), GTK_WIDGET(dialog->preview), TRUE, TRUE, 0);
+
+    /* Create controls box (right side) */
+    controls_vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 8);
+    gtk_widget_set_size_request(controls_vbox, 280, -1);
+    gtk_widget_set_margin_start(controls_vbox, 0);
+    gtk_widget_set_margin_end(controls_vbox, 0);
+    gtk_box_pack_start(GTK_BOX(main_hbox), controls_vbox, FALSE, FALSE, 0);
+
+    dialog->controls_box = controls_vbox;
+
+    /* Create control groups dynamically */
+    for (i = 0; i < num_controls; i++) {
+        GtkWidget *control_vbox;
+        GtkWidget *label;
+        GtkWidget *scale_hbox;
+        GtkWidget *scale;
+        GtkWidget *spin;
+        GtkAdjustment *adjustment;
+        gdouble step;
+
+        /* Initialize filter range to UI range if not specified */
+        if (dialog->params[i].filter_min == 0.0 && dialog->params[i].filter_max == 0.0 &&
+            dialog->params[i].min_value != 0.0) {
+            /* Only set defaults if filter range wasn't explicitly set */
+            /* We can't distinguish 0.0 from unset, so we'll require explicit setting */
+        }
+
+        /* Create vertical box for this control (no frame) */
+        control_vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+        gtk_widget_set_margin_bottom(control_vbox, 10);
+        gtk_box_pack_start(GTK_BOX(controls_vbox), control_vbox, FALSE, FALSE, 0);
+
+        /* Create label */
+        label = gtk_label_new(controls[i].label);
+        gtk_widget_set_halign(label, GTK_ALIGN_START);
+        gtk_widget_set_margin_bottom(label, 3);
+        gtk_box_pack_start(GTK_BOX(control_vbox), label, FALSE, FALSE, 0);
+
+        /* Create horizontal box for scale and spin */
+        scale_hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
+        gtk_box_pack_start(GTK_BOX(control_vbox), scale_hbox, TRUE, TRUE, 0);
+
+        /* Determine step value */
+        step = controls[i].step;
+        if (step == 0.0) {
+            /* Default step based on range */
+            gdouble range = controls[i].max_value - controls[i].min_value;
+            step = (range > 100.0) ? 1.0 : (range > 10.0) ? 0.1 : 0.01;
+        }
+
+        /* Create adjustment */
+        adjustment = gtk_adjustment_new(controls[i].default_value,
+                                        controls[i].min_value,
+                                        controls[i].max_value,
+                                        step,
+                                        step * 10.0,  /* Page step */
+                                        0.0);
+
+        /* Create scale (slider) */
+        scale = gtk_scale_new(GTK_ORIENTATION_HORIZONTAL, adjustment);
+        gtk_scale_set_digits(GTK_SCALE(scale), controls[i].decimals);
+        gtk_scale_set_draw_value(GTK_SCALE(scale), FALSE);
+        gtk_widget_set_hexpand(scale, TRUE);
+        gtk_widget_set_margin_end(scale, 5);
+        gtk_box_pack_start(GTK_BOX(scale_hbox), scale, TRUE, TRUE, 0);
+
+        /* Create spin button */
+        spin = gtk_spin_button_new(adjustment, step, controls[i].decimals);
+        gtk_widget_set_size_request(spin, 70, -1);
+        gtk_box_pack_start(GTK_BOX(scale_hbox), spin, FALSE, FALSE, 0);
+
+        /* Store widgets */
+        dialog->scale_widgets[i] = scale;
+        dialog->spin_widgets[i] = spin;
+
+        /* Connect signals for synchronization and preview updates */
+        g_signal_connect(scale, "value-changed",
+                        G_CALLBACK(on_scale_value_changed), dialog);
+        g_signal_connect(spin, "value-changed",
+                        G_CALLBACK(on_spin_value_changed), dialog);
+    }
+
+    /* Get button box from dialog (for OK/Cancel) */
+    /* Note: OK/Cancel buttons are already added by gtk_dialog_new_with_buttons */
+    /* gtk_dialog_get_action_area is deprecated but still works in GTK3 */
+    #ifdef __GNUC__
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+    #endif
+    button_box = gtk_dialog_get_action_area(GTK_DIALOG(dialog->dialog));
+    #ifdef __GNUC__
+    #pragma GCC diagnostic pop
+    #endif
+    if (button_box) {
+        gtk_widget_set_margin_top(button_box, 5);
+        gtk_widget_set_margin_bottom(button_box, 5);
+        gtk_widget_set_margin_start(button_box, 5);
+        gtk_widget_set_margin_end(button_box, 5);
+        
+        /* Make action area expand horizontally to fill width */
+        gtk_widget_set_hexpand(button_box, TRUE);
+        
+        /* Create reset button and add it to the left side of action area */
+        reset_button = gtk_button_new_from_icon_name("view-refresh", GTK_ICON_SIZE_BUTTON);
+        gtk_button_set_label(GTK_BUTTON(reset_button), "Reset");
+        gtk_widget_set_halign(reset_button, GTK_ALIGN_START);
+        gtk_box_pack_start(GTK_BOX(button_box), reset_button, FALSE, FALSE, 0);
+        gtk_box_reorder_child(GTK_BOX(button_box), reset_button, 0);  /* Move to start */
+        g_signal_connect(reset_button, "clicked",
+                        G_CALLBACK(on_reset_clicked), dialog);
+    }
+
+    /* Show all widgets */
+    gtk_widget_show_all(content_area);
+
+    return dialog;
+}
+
+/**
+ * Set the image layers for the preview widget
+ */
+void filter_dialog_set_layers(FilterDialog *dialog,
+                               ImageLayer *before_layer,
+                               ImageLayer *after_layer)
+{
+    cairo_surface_t *before_surface = NULL;
+    cairo_surface_t *after_surface = NULL;
+
+    if (!dialog || !dialog->preview) {
+        return;
+    }
+
+    /* Get composite surfaces from layers if available */
+    if (before_layer && before_layer->surface) {
+        before_surface = cairo_surface_reference(before_layer->surface);
+    }
+
+    if (after_layer && after_layer->surface) {
+        after_surface = cairo_surface_reference(after_layer->surface);
+    }
+
+    filter_preview_set_before_surface(dialog->preview, before_surface);
+    filter_preview_set_after_surface(dialog->preview, after_surface);
+
+    /* Clean up references */
+    if (before_surface) {
+        cairo_surface_destroy(before_surface);
+    }
+    if (after_surface) {
+        cairo_surface_destroy(after_surface);
+    }
+}
+
+/**
+ * Update the after layer preview
+ */
+void filter_dialog_update_after_layer(FilterDialog *dialog, ImageLayer *after_layer)
+{
+    cairo_surface_t *after_surface = NULL;
+
+    if (!dialog || !dialog->preview) {
+        return;
+    }
+
+    if (after_layer && after_layer->surface) {
+        after_surface = cairo_surface_reference(after_layer->surface);
+    }
+
+    filter_preview_set_after_surface(dialog->preview, after_surface);
+
+    if (after_surface) {
+        cairo_surface_destroy(after_surface);
+    }
+}
+
+/**
+ * Get the dialog window widget
+ */
+GtkWindow* filter_dialog_get_window(FilterDialog *dialog)
+{
+    if (!dialog || !dialog->dialog) {
+        return NULL;
+    }
+
+    return GTK_WINDOW(dialog->dialog);
+}
+
+/**
+ * Run the dialog and get control values
+ */
+gint filter_dialog_run(FilterDialog *dialog,
+                       GtkWindow *parent,
+                       gdouble *values,
+                       gint num_values)
+{
+    gint response;
+
+    if (!dialog || !values || num_values <= 0) {
+        return GTK_RESPONSE_CANCEL;
+    }
+
+    if (parent) {
+        gtk_window_set_transient_for(filter_dialog_get_window(dialog), parent);
+    }
+
+    response = gtk_dialog_run(GTK_DIALOG(dialog->dialog));
+
+    if (response == GTK_RESPONSE_OK) {
+        filter_dialog_get_values(dialog, values, num_values);
+    }
+
+    return response;
+}
+
+/**
+ * Get current control values
+ */
+void filter_dialog_get_values(FilterDialog *dialog,
+                              gdouble *values,
+                              gint num_values)
+{
+    gint i;
+    gint count;
+
+    if (!dialog || !values || num_values <= 0) {
+        return;
+    }
+
+    count = (num_values < dialog->num_controls) ? num_values : dialog->num_controls;
+
+    for (i = 0; i < count; i++) {
+        if (dialog->spin_widgets[i]) {
+            values[i] = gtk_spin_button_get_value(GTK_SPIN_BUTTON(dialog->spin_widgets[i]));
+        } else {
+            values[i] = dialog->params[i].default_value;
+        }
+    }
+}
+
+/**
+ * Set callback function for live preview updates
+ */
+void filter_dialog_set_preview_callback(FilterDialog *dialog,
+                                        FilterDialogPreviewCallback callback,
+                                        gpointer user_data)
+{
+    if (!dialog) {
+        return;
+    }
+
+    dialog->preview_callback = callback;
+    dialog->preview_user_data = user_data;
+}
+
+/**
+ * Reset all controls to default values
+ */
+void filter_dialog_reset(FilterDialog *dialog)
+{
+    gint i;
+
+    if (!dialog) {
+        return;
+    }
+
+    for (i = 0; i < dialog->num_controls; i++) {
+        if (dialog->spin_widgets[i]) {
+            gtk_spin_button_set_value(GTK_SPIN_BUTTON(dialog->spin_widgets[i]),
+                                     dialog->params[i].default_value);
+        }
+    }
+}
+
+/**
+ * Free the filter dialog
+ */
+void filter_dialog_free(FilterDialog *dialog)
+{
+    gint i;
+
+    if (!dialog) {
+        return;
+    }
+
+    if (dialog->dialog) {
+        gtk_widget_destroy(dialog->dialog);
+    }
+
+    if (dialog->scale_widgets) {
+        g_free(dialog->scale_widgets);
+    }
+
+    if (dialog->spin_widgets) {
+        g_free(dialog->spin_widgets);
+    }
+
+    if (dialog->params) {
+        g_free(dialog->params);
+    }
+
+    g_free(dialog);
+}
+
