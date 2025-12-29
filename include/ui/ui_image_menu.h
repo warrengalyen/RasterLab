19943143630d83b4592dca9f4c4e594b1ed76f8c
warrengalyen <wgalyen@hotmail.com>
2025-12-29 11:06:32 -0800

    Refactor Image menu callbacks

---
+++ include/ui/ui_image_menu.h
@@ -0,0 +1,25 @@
+#ifndef UI_IMAGE_MENU_H
+#define UI_IMAGE_MENU_H
+
+#include "ui.h"
+#include <gtk/gtk.h>
+
+/**
+ * Image menu callback functions
+ */
+void on_image_canvas_size(GtkWidget* widget, gpointer data);
+void on_image_duplicate(GtkWidget* widget, gpointer data);
+void on_image_fit_active_layer(GtkWidget* widget, gpointer data);
+void on_image_fit_all_layers(GtkWidget* widget, gpointer data);
+void on_image_flip_horizontal(GtkWidget* widget, gpointer data);
+void on_image_flip_vertical(GtkWidget* widget, gpointer data);
+void on_image_transpose(GtkWidget* widget, gpointer data);
+void on_image_merge_visible(GtkWidget* widget, gpointer data);
+void on_image_flatten(GtkWidget* widget, gpointer data);
+
+/**
+ * Setup Image menu from Glade builder
+ */
+void ui_image_menu_setup(GtkBuilder* builder, AppContext* ctx);
+
+#endif /* UI_IMAGE_MENU_H */
