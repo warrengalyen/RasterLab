3cdc43fa07afb316635618902d124d99e0f3c4eb
warrengalyen <wgalyen@hotmail.com>
2025-12-29 12:13:54 -0800

    Refactor Select menu callbacks

---
+++ include/ui/ui_select_menu.h
@@ -0,0 +1,24 @@
+#ifndef UI_SELECT_MENU_H
+#define UI_SELECT_MENU_H
+
+#include "ui.h"
+#include <gtk/gtk.h>
+
+/**
+ * Select menu callback functions
+ */
+void on_select_all(GtkMenuItem* menu_item, gpointer user_data);
+void on_select_none(GtkMenuItem* menu_item, gpointer user_data);
+void on_select_invert(GtkMenuItem* menu_item, gpointer user_data);
+void on_select_grow(GtkMenuItem* menu_item, gpointer user_data);
+void on_select_shrink(GtkMenuItem* menu_item, gpointer user_data);
+void on_select_border(GtkMenuItem* menu_item, gpointer user_data);
+void on_select_feather(GtkMenuItem* menu_item, gpointer user_data);
+void on_select_sharpen(GtkMenuItem* menu_item, gpointer user_data);
+
+/**
+ * Setup Select menu from Glade builder
+ */
+void ui_select_menu_setup(GtkBuilder* builder, AppContext* ctx, GtkAccelGroup* accel_group);
+
+#endif /* UI_SELECT_MENU_H */
