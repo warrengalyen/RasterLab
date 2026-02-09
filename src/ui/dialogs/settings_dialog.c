ea50920f525a4aaa5187c2fe374ee1a10b10b471
warrengalyen <wgalyen@hotmail.com>
2026-02-09 14:13:58 -0800

    Add rest of current user settings to dialog

--- src/ui/dialogs/settings_dialog.c
+++ src/ui/dialogs/settings_dialog.c
@@ -1,15 +1,23 @@
 /*
- * Settings dialog - canvas background, undo, performance, recent files
+ * Settings dialog 
  * Uses layout from resources/ui/settings_dialog.glade
  */
 #include "ui/dialogs/settings_dialog.h"
+#include "app/autosave.h"
 #include "app/settings.h"
+#include "render/gpu_compositor.h"
 #include "ui.h"
 #include "ui/dialogs/color_chooser_dialog.h"
 #include "ui/ui_utils.h"
 #include <glib.h>
 #include <gtk/gtk.h>
+#include <string.h>
 
+enum {
+    RENDERER_COLUMN_LABEL,
+    RENDERER_COLUMN_DEVICE_ID,
+    RENDERER_N_COLUMNS
+};
 
 /* Apply dialog values to settings and save; then update UI (e.g. canvas bg) */
 static void settings_dialog_apply_and_save(GtkDialog* dialog, AppContext* ctx) {
@@ -46,6 +54,33 @@ static void settings_dialog_apply_and_save(GtkDialog* dialog, AppContext* ctx) {
     if (recent_adj) {
         guint val = (guint)gtk_adjustment_get_value(recent_adj);
         settings_set_max_recent_files(ctx->settings, val);
+    }
+
+    /* File recovery interval (30-2700 seconds) */
+    GtkAdjustment* recovery_adj = GTK_ADJUSTMENT(gtk_builder_get_object(builder, "file_recovery_interval_adjustment"));
+    if (recovery_adj) {
+        gint sec = (gint)gtk_adjustment_get_value(recovery_adj);
+        settings_set_file_recovery_interval_seconds(ctx->settings, sec);
+        autosave_set_interval((guint)sec);
+    }
+
+    /* Renderer (GPU device): empty string = system default */
+    GtkComboBox* renderer_combo = GTK_COMBO_BOX(gtk_builder_get_object(builder, "renderer_combobox"));
+    if (renderer_combo) {
+        GtkTreeIter iter;
+        if (gtk_combo_box_get_active_iter(renderer_combo, &iter)) {
+            GtkTreeModel* model = gtk_combo_box_get_model(renderer_combo);
+            gchar* device_id = NULL;
+            gtk_tree_model_get(model, &iter, RENDERER_COLUMN_DEVICE_ID, &device_id, -1);
+            settings_set_gpu_device_name(ctx->settings, device_id && device_id[0] != '\0' ? device_id : NULL);
+            g_free(device_id);
+        }
+    }
+
+    /* Hardware acceleration */
+    GtkToggleButton* gpu_check = GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder, "hardware_acceleration_checkbox"));
+    if (gpu_check) {
+        settings_set_gpu_acceleration_enabled(ctx->settings, gtk_toggle_button_get_active(gpu_check));
     }
 
     if (ctx->app_dir) {
@@ -143,6 +178,26 @@ void settings_dialog_show(AppContext* ctx) {
     g_object_set_data_full(G_OBJECT(dialog), "settings_builder", g_object_ref(builder), (GDestroyNotify)g_object_unref);
     g_object_unref(builder); /* dialog owns one ref via data */
 
+    /* Prefix labels for settings that require an app restart to take effect */
+    {
+        const char* restart_label_ids[] = {
+            "undo_limit_label",
+            "undo_compression_level_label",
+            "threads_label",
+            "renderer_label",
+            "hardware_acceleration_label",
+            NULL};
+        for (const char** id = restart_label_ids; *id != NULL; id++) {
+            GtkLabel* label = GTK_LABEL(gtk_builder_get_object(builder, *id));
+            if (label) {
+                const gchar* current = gtk_label_get_text(label);
+                gchar* prefixed = g_strdup_printf("*%s", current ? current : "");
+                gtk_label_set_text(label, prefixed);
+                g_free(prefixed);
+            }
+        }
+    }
+
     /* Canvas background color: plain button that opens custom color chooser */
     GtkWidget* canvas_bg_btn = GTK_WIDGET(gtk_builder_get_object(builder, "canvas_bgcolor_button"));
     if (canvas_bg_btn) {
@@ -196,6 +251,67 @@ void settings_dialog_show(AppContext* ctx) {
     if (recent_adj) {
         guint max_rf = settings_get_max_recent_files(ctx->settings);
         gtk_adjustment_set_value(recent_adj, (gdouble)max_rf);
+    }
+
+    /* File recovery interval */
+    GtkAdjustment* recovery_adj = GTK_ADJUSTMENT(gtk_builder_get_object(builder, "file_recovery_interval_adjustment"));
+    if (recovery_adj) {
+        gint sec = settings_get_file_recovery_interval_seconds(ctx->settings);
+        gtk_adjustment_set_value(recovery_adj, (gdouble)sec);
+    }
+
+    /* Renderer combobox: populate with available GPUs; disable if none */
+    GtkWidget* renderer_combo = GTK_WIDGET(gtk_builder_get_object(builder, "renderer_combobox"));
+    GtkWidget* gpu_checkbox = GTK_WIDGET(gtk_builder_get_object(builder, "hardware_acceleration_checkbox"));
+    gint gpu_count = 0;
+    GPUDeviceInfo* gpu_list = gpu_compositor_get_device_list(&gpu_count);
+    if (gpu_count > 0 && gpu_list && renderer_combo) {
+        GtkListStore* store = gtk_list_store_new(RENDERER_N_COLUMNS, G_TYPE_STRING, G_TYPE_STRING);
+        for (gint i = 0; i < gpu_count; i++) {
+            GtkTreeIter iter;
+            gchar* label = gpu_list[i].is_default && gpu_list[i].name
+                               ? g_strdup_printf("Default (%s)", gpu_list[i].name)
+                               : g_strdup(gpu_list[i].name ? gpu_list[i].name : "Unknown");
+            gchar* device_id = gpu_list[i].is_default ? g_strdup("") : g_strdup(gpu_list[i].name ? gpu_list[i].name : "");
+            gtk_list_store_append(store, &iter);
+            gtk_list_store_set(store, &iter, RENDERER_COLUMN_LABEL, label, RENDERER_COLUMN_DEVICE_ID, device_id, -1);
+            g_free(label);
+            g_free(device_id);
+        }
+        gtk_combo_box_set_model(GTK_COMBO_BOX(renderer_combo), GTK_TREE_MODEL(store));
+        g_object_unref(store);
+        GtkCellRenderer* cell = gtk_cell_renderer_text_new();
+        gtk_cell_layout_clear(GTK_CELL_LAYOUT(renderer_combo));
+        gtk_cell_layout_pack_start(GTK_CELL_LAYOUT(renderer_combo), cell, TRUE);
+        gtk_cell_layout_set_attributes(GTK_CELL_LAYOUT(renderer_combo), cell, "text", RENDERER_COLUMN_LABEL, NULL);
+        /* Select row matching current gpu_device setting (empty/NULL = default = first row) */
+        const gchar* current = settings_get_gpu_device_name(ctx->settings);
+        gint select = 0;
+        if (current && current[0] != '\0') {
+            for (gint i = 0; i < gpu_count; i++) {
+                if (!gpu_list[i].is_default && gpu_list[i].name && strcmp(current, gpu_list[i].name) == 0) {
+                    select = i;
+                    break;
+                }
+            }
+        }
+        gtk_combo_box_set_active(GTK_COMBO_BOX(renderer_combo), select);
+        gpu_compositor_free_device_list(gpu_list, gpu_count);
+    } else {
+        if (renderer_combo) {
+            gtk_widget_set_sensitive(renderer_combo, FALSE);
+        }
+        if (gpu_checkbox) {
+            gtk_widget_set_sensitive(gpu_checkbox, FALSE);
+        }
+        if (gpu_list) {
+            gpu_compositor_free_device_list(gpu_list, gpu_count);
+        }
+    }
+
+    /* Hardware acceleration checkbox */
+    if (gpu_checkbox) {
+        gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(gpu_checkbox), settings_get_gpu_acceleration_enabled(ctx->settings));
     }
 
     GtkWidget* ok_btn = GTK_WIDGET(gtk_builder_get_object(builder, "settings_ok_button"));
