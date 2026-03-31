0867122f3039dc663829848cc3d213effb3c11dd
warrengalyen <wgalyen@hotmail.com>
2026-03-31 10:05:26 -0700

    Only copy shared library files to bin folder if newer

--- cmake/CopyLibpngDLL.cmake
+++ cmake/CopyLibpngDLL.cmake
@@ -1,5 +1,6 @@
-# CMake script to copy libpng DLL if it exists
-# This script is called from ExternalProject POST_BUILD to copy the DLL
+# CMake script to copy libpng DLL if it exists.
+# Called from ExternalProject POST_BUILD. Uses copy_if_different so we do not
+# overwrite bin when the file is already identical (avoids needless copies).
 
 set(LIBPNG_INSTALL_DIR "@LIBPNG_INSTALL_DIR@")
 set(LIBPNG_DLL_NAME "@LIBPNG_DLL_NAME@")
@@ -10,12 +11,22 @@ set(DLL_BIN_PATH "${LIBPNG_INSTALL_DIR}/bin/${LIBPNG_DLL_NAME}")
 set(DLL_LIB_PATH "${LIBPNG_INSTALL_DIR}/lib/${LIBPNG_DLL_NAME}")
 
 if(EXISTS "${DLL_BIN_PATH}")
-    message(STATUS "Copying libpng DLL from bin/: ${DLL_BIN_PATH}")
-    file(COPY "${DLL_BIN_PATH}" DESTINATION "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}")
+    set(LIBPNG_DLL_SRC "${DLL_BIN_PATH}")
 elseif(EXISTS "${DLL_LIB_PATH}")
-    message(STATUS "Copying libpng DLL from lib/: ${DLL_LIB_PATH}")
-    file(COPY "${DLL_LIB_PATH}" DESTINATION "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}")
+    set(LIBPNG_DLL_SRC "${DLL_LIB_PATH}")
 else()
     message(WARNING "libpng DLL not found in ${LIBPNG_INSTALL_DIR}/bin/ or ${LIBPNG_INSTALL_DIR}/lib/")
+    return()
+endif()
+
+file(MAKE_DIRECTORY "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}")
+execute_process(
+    COMMAND "${CMAKE_COMMAND}" -E copy_if_different
+        "${LIBPNG_DLL_SRC}"
+        "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/"
+    RESULT_VARIABLE _libpng_copy_rv
+)
+if(NOT _libpng_copy_rv EQUAL 0)
+    message(WARNING "libpng: copy_if_different failed for ${LIBPNG_DLL_SRC}")
 endif()
 
--- cmake/CopyOpenEXRDLLs.cmake
+++ cmake/CopyOpenEXRDLLs.cmake
@@ -1,16 +1,27 @@
 # CMake script to copy OpenEXR (and Imath) DLLs/shared libs to bin directory.
 # Called from ExternalProject POST_BUILD. Compatible with CMake 3.10+.
+# Uses copy_if_different so files already present in bin with identical content
+# are not copied again.
 
 set(OPENEXR_INSTALL_DIR "@OPENEXR_INSTALL_DIR@")
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "@CMAKE_RUNTIME_OUTPUT_DIRECTORY@")
+
+file(MAKE_DIRECTORY "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}")
 
 if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows")
     set(OPENEXR_BIN "${OPENEXR_INSTALL_DIR}/bin")
     if(EXISTS "${OPENEXR_BIN}")
         file(GLOB OPENEXR_DLLS "${OPENEXR_BIN}/*.dll")
         foreach(DLL ${OPENEXR_DLLS})
-            file(COPY "${DLL}" DESTINATION "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}")
-            message(STATUS "Copying OpenEXR/Imath DLL: ${DLL}")
+            execute_process(
+                COMMAND "${CMAKE_COMMAND}" -E copy_if_different
+                    "${DLL}"
+                    "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/"
+                RESULT_VARIABLE _openexr_copy_rv
+            )
+            if(NOT _openexr_copy_rv EQUAL 0)
+                message(WARNING "OpenEXR: copy_if_different failed for ${DLL}")
+            endif()
         endforeach()
     endif()
 else()
@@ -18,8 +29,15 @@ else()
     if(EXISTS "${OPENEXR_LIB}")
         file(GLOB OPENEXR_SOS "${OPENEXR_LIB}/*.so" "${OPENEXR_LIB}/*.so.*" "${OPENEXR_LIB}/*.dylib" "${OPENEXR_LIB}/*.dylib.*")
         foreach(LIB ${OPENEXR_SOS})
-            file(COPY "${LIB}" DESTINATION "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}")
-            message(STATUS "Copying OpenEXR/Imath shared lib: ${LIB}")
+            execute_process(
+                COMMAND "${CMAKE_COMMAND}" -E copy_if_different
+                    "${LIB}"
+                    "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/"
+                RESULT_VARIABLE _openexr_copy_rv
+            )
+            if(NOT _openexr_copy_rv EQUAL 0)
+                message(WARNING "OpenEXR: copy_if_different failed for ${LIB}")
+            endif()
         endforeach()
     endif()
 endif()
